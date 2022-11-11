3fad809b8d299fe871b4b23e18bcdee4d2181e0b
Rosshoven <Michaeledwardross@gmail.com>
2022-11-11 14:04:56 -0600

    making Image folder

--- mapanimation.js
+++ mapanimation.js
@@ -34,15 +34,6 @@
             setTimeout(addMarkers, 15000);
         }
         
-        // async function run(){
-        //     // get bus data    
-        // 	const locations = await getBusLocations();
-        // 	console.log(new Date());
-        // 	console.log(locations);
-        
-        // 	// timer
-        // 	setTimeout(run, 15000);
-        // }
         
         // Request bus data from MBTA
         async function getBusLocations(){
